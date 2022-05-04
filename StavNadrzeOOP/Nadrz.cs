6f5a016d97bb861ee4d8cae89bc3f6680f6d967a
Veronika Mareckova <m-veronika@email.cz>
2022-05-04 17:32:17 +0200

    Přidejte soubory projektu.

---
+++ StavNadrzeOOP/Nadrz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StavNadrzeOOP
+{
+    class Nadrz
+    {
+        //atributy
+        private uint objem; //kolik se vejde
+        uint stav; //private  // kolik paliva je v nadrzi
+
+        //Konstruktor
+        public Nadrz()
+        {
+            this.objem = 50;
+            stav = 0;
+        }
+        //konstruktor - pretizeni metody
+        public Nadrz(uint novyObjem)
+        {
+            this.objem = novyObjem;
+            stav = 0;
+        }
+
+        //Get metody
+        public uint GetStav()
+        {
+            return stav;
+        }
+
+        public uint GetObjem()
+        {
+            return objem;
+        }
+
+        //Metody
+       public void Pridej(uint kolik)
+        {
+            if (stav + kolik > objem)
+                return; //dalsi kod v metode se dal neprovede
+
+            stav = stav + kolik; //stav += kolik;
+        }
+                
+        public void Odeber(uint kolik)
+        {
+            if (stav < kolik)
+                return;
+            stav = stav - kolik; //stav -= kolik;
+        }
+
+    }
+}
---
+++ StavNadrzeOOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StavNadrzeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nadrz NadrzBeznehoAuta = new Nadrz();
+            Console.WriteLine(NadrzBeznehoAuta.GetObjem());
+
+            Nadrz NadrzAuta = new Nadrz(500);
+            Console.WriteLine(NadrzAuta.GetObjem());
+
+         /*
+         * atrituby
+         * Get metody (stav objem)
+         * metody PRIDEJ ODEBER
+         *  
+         *  metoda  PRIDEJ overi podminkou zda se vejde  polivo do nadrze
+         *  metoda  ODEBER overi podminkou zda jde pozadovane mnozsvi odebrat
+         * 
+         * Konstruktor 
+         * pozn. na zacatku neni OBJEKT nastaven.
+         * pozn. Konstruktor pomaha OBJEKT inicializovat (pomaha objekt vytvaret)
+         */
+
+        }
+    }
+}
